d2545db1662ae5f3cbb18a081f8f5e7f28d0ab74
Roiprojects <roiprojects012@gmail.com>
2026-04-17 21:21:17 +0530

    Update next.config.ts to modify build settings
    Removed devIndicators and added TypeScript and ESLint configurations to ignore build errors.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
+    typescript: {
+          ignoreBuildErrors: true,
+    },
+    eslint: {
+          ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
